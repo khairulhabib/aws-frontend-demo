fbedac44d619296648b1054888a09d9cd76ffaf8
Habib Muttaqien <mkhabib@88665a25e836.ant.amazon.com>
2022-08-22 16:05:39 +0700

    change content

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,5 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Cloud Day Demo ID 2022' });
 });
 
-const btnIncrease = document.getElementById('increase');
-btnIncrease.addEventListener('click',function(e){
-  console.log('increase clicked')
-});
-const btnDecrease = document.getElementById('decrease');
-btnIncrease.addEventListener('click',function(e){
-  console.log('decrease clicked')
-});
-
 
 module.exports = router;
