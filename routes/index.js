500ebf49a2a5bc92d0eaa91e4119a862f973456a
Habib Muttaqien <mkhabib@88665a25e836.ant.amazon.com>
2022-08-22 17:19:17 +0700

    change content

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var url = "https://4d8lzogr7j.execute-api.ap-southeast-1.amazonaws.com/dev/";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Cloud Day Demo ID 2022' });
 });
 router.post('/', function (req, res) {
-  console.log(req.body.control);
+  var control = req.body.control;
+  postData();
   res.send(req.body.control);
 });
+
+async function postData(){
+  const response = await fetch(url,{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+  return response.json();
+}
 
 module.exports = router;
